4506d6262d0871c6f3aa363ea7ce0028ea794980
logeek <dengbinemmm@gmail.com>
2022-04-12 09:40:37 +0800

    修改 FILE_HOST

--- src/main/java/com/db/sfs/common/GlobalVars.java
+++ src/main/java/com/db/sfs/common/GlobalVars.java
@@ -4,6 +4,6 @@ import java.io.File;
 
 public class GlobalVars {
     public static final String BASE_DIR = "D:\\EdgeDownload\\";
-    public static final String FILE_HOST = "http://150.158.18.29/";
+    public static final String FILE_HOST = "http://150.158.18.29/webstatic/";
     public static final String FILE_SEP = File.separator;
 }
