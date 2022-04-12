d2fc232ca1cc97c858987a0ae39464027e4f5ee4
logeek <dengbinemmm@gmail.com>
2022-04-12 11:12:04 +0800

    修改默认失败状态码为999

--- src/main/java/com/db/sfs/common/StatusCode.java
+++ src/main/java/com/db/sfs/common/StatusCode.java
@@ -4,5 +4,5 @@ public class StatusCode {
     // 默认的成功状态码
     public static final Integer OK = 200;
     // 默认的失败状态码
-    public static final Integer ERROR = 500;
+    public static final Integer ERROR = 999;
 }
